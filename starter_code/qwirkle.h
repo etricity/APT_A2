d784acd084d52e60d61a5a9f85a98b9da8b62b2e
Isaiah Cuzzupe <upmatter@hotmail.com>
2019-09-15 14:16:13 +1000

    Menu Added- small amounts of validation

---
+++ starter_code/qwirkle.h
@@ -0,0 +1,18 @@
+#include "LinkedList.h"
+#include <iostream>
+
+#define EXIT_SUCCESS    0
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+void printMenu();
+
+void newGame();
+void loadGame();
+void showInfo();
+//void quit();
+
+string userInput;
